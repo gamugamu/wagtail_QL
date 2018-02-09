63b18018dd6614d8ab500d76403eff30c8dcf0dc
ABADIE Loïc <loic.abadie@pictime.com>
2018-02-09 10:46:15 +0100

    [preparation migration]

--- mysite/schema/schema.py
+++ mysite/schema/schema.py
@@ -3,7 +3,7 @@ from graphene_django import DjangoObjectType
 from blog.models import BlogPage, BlogPageGalleryImage
 import graphene
 from django.core.files.storage import default_storage
-from  django.core.files.storage import DefaultStorage
+from django.core.files.storage import DefaultStorage
 
 
 class BlogPageGalleryImage_(DjangoObjectType):
@@ -15,14 +15,37 @@ class BlogPageGalleryImage_(DjangoObjectType):
     def resolve_url(self, info):
         return self.image.file.url
 
-class BlogPage_(DjangoObjectType):
+class Blog(DjangoObjectType):
     class Meta:
         model = BlogPage
 
-class Query(graphene.ObjectType):
-    blog = graphene.List(BlogPage_)
+class CreateBlog(graphene.Mutation):
+    ok   = graphene.Boolean()
+    blog = graphene.Field(lambda: Blog)
 
-    def resolve_blog(self, info):
+    class Arguments:
+        intro = graphene.String()
+
+    def mutate(self, info, **args):
+        print "======================= **** will mutate", args
+        blog = BlogPage(intro=args["intro"])
+        blog.save()
+        print "******** ", blog.id
+        ok    = True
+        return CreateBlog(blog=blog, ok=ok)
+
+class Query(graphene.ObjectType):
+    blog_all    = graphene.List(Blog)
+    blog        = graphene.Field(Blog, id=graphene.Int())
+
+    def resolve_blog_all(self, info):
         return BlogPage.objects.all()
 
-schema = graphene.Schema(query=Query)
+    def resolve_blog(self, info, **args):
+        print "info", args
+        return BlogPage.objects.get(id=args["id"])
+
+class Mutation(graphene.ObjectType):
+    create_blog = CreateBlog.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
