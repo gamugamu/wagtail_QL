6e8da37366aff9fd5aaccd1975cd9948ad21b825
ABADIE Loïc <loic.abadie@pictime.com>
2018-02-15 16:28:18 +0100

    [node react- front-server]

--- flask_sqlalchemy/test.py
+++ flask_sqlalchemy/test.py
@@ -9,7 +9,6 @@ bucket = client.get_bucket('wagtailqlbucket')
 # note: ne gere pas les operations concurrentes. Le service
 # est utilisé par une ou deux personnes max.
 def upload_to_default_bucket(file):
-    print "uploading file***"
     uuid_   = uuid.uuid4().hex
     blob    = Blob('upload/' + uuid_, bucket)
     blob.content_type = "image/jpeg"
@@ -19,11 +18,12 @@ def upload_to_default_bucket(file):
 
 def synch_from_default_bucket(uid_file, file_name):
     blob = bucket.get_blob("upload/" + uid_file)
-    print "blob for ", uid_file, blob, blob.public_url
+
     if blob == None:
+        # Ne devrait pas si le user récupère le bon id.
         return ""
     else:
         perm_blob = bucket.copy_blob(blob, bucket, new_name="upload/" + file_name + uid_file)
         bucket.delete_blobs([blob])
-        
+
         return perm_blob.public_url
