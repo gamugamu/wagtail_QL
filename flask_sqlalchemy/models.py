cee7d13be5a3379a32412be089c7d08b3af1a3d0
ABADIE Loïc <loic.abadie@pictime.com>
2018-02-26 16:01:04 +0100

    [clean + refacto]

--- flask_sqlalchemy/models.py
+++ flask_sqlalchemy/models.py
@@ -15,40 +15,6 @@ Base = declarative_base()
 # We will need this for querying
 Base.query = db_session.query_property()
 
-class Department(Base):
-    __tablename__   = 'department'
-    id              = Column(Integer, primary_key=True)
-    name            = Column(String)
-
-class Employee(Base):
-    __tablename__   = 'employee'
-    id              = Column(Integer, primary_key=True)
-    name            = Column(String)
-    hired_on        = Column(DateTime, default=func.now())
-    department_id   = Column(Integer, ForeignKey('department.id'))
-    department      = relationship(
-                        Department,
-                        backref=backref('employees',
-                        uselist=True,
-                        cascade='delete,all'))
-
-class User(Base):
-    __tablename__   = 'user'
-    id              = Column(Integer, primary_key=True)
-    name            = Column(String)
-    last_name       = Column(String)
-    pets            = relationship("Pet", back_populates="user")
-
-
-class Pet(Base):
-    __tablename__   = 'pet'
-    id              = Column(Integer, primary_key=True)
-    name            = Column(String)
-    user_id         = Column(Integer, ForeignKey('user.id'))
-    user            = relationship("User", back_populates="pets")
-    url_avatar      = Column(String)
-
-##########################
 class Pma_base(Base):
     __tablename__   = 'pmabase'
     id              = Column(Integer, primary_key=True)
--- flask_sqlalchemy/schema.py
+++ flask_sqlalchemy/schema.py
@@ -2,31 +2,9 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
-from models import db_session, User as UserModel, DBHelper, Pet as PetModel, Pma_home as Pma_homeModel, Pma_base
+from models import db_session, Pma_home as Pma_homeModel, Pma_base
 from dateutil import parser
 import time
-
-class Pet(SQLAlchemyObjectType):
-    class Meta:
-        model = PetModel
-
-class User(SQLAlchemyObjectType):
-    class Meta:
-        model = UserModel
-
-class PetField(graphene.InputObjectType):
-    name                = graphene.String(required=True)
-    link_uuid_avatar    = graphene.String()
-
-# helper
-def helper_create_pet(pet_field, user):
-    pet = PetModel(name=pet_field.name)
-
-    #if pet_field.link_uuid_avatar is not None:
-    #    path            = synch_from_default_bucket(pet_field.link_uuid_avatar, "pet_name")
-    #    pet.url_avatar  = path
-
-    pet.user = user
 
 ##################### helper ######################
 
@@ -75,6 +53,7 @@ class Pma_base_input(graphene.InputObjectType):
     id              = graphene.Int()
     category        = Category_language()
     is_active       = graphene.Boolean()
+
 ################### HOME_PMA #####################
 
 class Pma_home(SQLAlchemyObjectType):
@@ -118,53 +97,14 @@ class Mutate_Pma_home(graphene.Mutation):
         return Mutate_Pma_home(pma=pma)
 
 ###################################################
-
-class AddNewPet(graphene.Mutation):
-    class Arguments:
-        add_pet     = PetField(required=True)
-        to_user_id  = graphene.Int(required=True)
-
-    pet = graphene.Field(lambda: Pet)
-
-    def mutate(self, info, add_pet=None, to_user_id=0):
-        user = db_session.query(UserModel).filter(UserModel.id == to_user_id).first()
-        helper_create_pet(add_pet, user)
-        DBHelper.fast_commit(pet)
-
-        return AddNewPet(pet=pet)
-
-class CreateUser(graphene.Mutation):
-    class Arguments:
-        name        = graphene.String(required=True)
-        add_pet     = PetField(required=False)
-
-    user = graphene.Field(lambda: User)
-
-    @staticmethod
-    def mutate(self, info, name, add_pet=None, file=None):
-        user = UserModel(name=name)
-
-        if add_pet is not None:
-            helper_create_pet(add_pet, user)
-
-        DBHelper.fast_commit(user)
-        return CreateUser(user=user)
-
 class Query(graphene.ObjectType):
-    all_users   = graphene.List(User)
     all_pmaHome = graphene.List(Pma_home)
-
-    def resolve_all_users(self, info, **args):
-        query = User.get_query(info)  # SQLAlchemy query
-        return query.all()
 
     def resolve_all_pmaHome(self, info, **args):
         query = Pma_home.get_query(info)  # SQLAlchemy query
         return query.all()
 
 class Mutation(graphene.ObjectType):
-    create_user = CreateUser.Field()
-    add_new_pet = AddNewPet.Field()
     mutate_Pma_home = Mutate_Pma_home.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
